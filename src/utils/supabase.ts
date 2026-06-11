c3a467cef183c2e4814a9050ad74232ea4abb024
Liam Texeira <liamtex1@gmail.com>
2026-06-10 22:04:03 -0300

    Se cambiaron algunas cosas por expo-image, se habia bajado la version de SDK

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,14 +1,15 @@
-import AsyncStorage from '@react-native-async-storage/async-storage'
-import { createClient } from '@supabase/supabase-js'
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { createClient } from "@supabase/supabase-js";
+import { Platform } from "react-native";
 
-export const supabase = createClient(
-  process.env.EXPO_PUBLIC_SUPABASE_URL!,
-  process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!,
-  {
-    auth: {
-      storage: AsyncStorage,
-      autoRefreshToken: true,
-      persistSession: true,
-      detectSessionInUrl: false,
-    },
-  })
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: Platform.OS === "web" ? undefined : AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: Platform.OS !== "web",
+    detectSessionInUrl: false,
+  },
+});
